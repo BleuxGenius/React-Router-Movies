31efc6ad285604509748233f3b718f0c6195fb66
Dani Blackwell <bleuxgenius@gmail.com>
2019-12-18 16:53:58 -0600

    still has error
    Mvp i think still has error

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
-const SavedList = props => (
+export default class SavedList extends Component {
+  constructor(props) {
+    super(props);
+    
+    
+    }
+render() {
+return (
+
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
+    {this.props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <Link to="/" className="home-button">
+      Home</Link>
   </div>
 );
-
-export default SavedList;
+    }
+  }
